142613717b7ae61c165f8227bbfab4ffee993fd1
srgriff <motherofgriffiths@gmail.com>
2025-10-15 18:34:26 -0400

    Update callbackVersion.js
    2

--- callbackVersion.js
+++ callbackVersion.js
@@ -10,7 +10,7 @@ function mowYard(name, callback) {
 
 function weedEat(name, callback) {
   setTimeout(() => {
-    // Check if person fell asleep (random chance increases with fatigue)
+    
     if (Math.random() < fatigueLevel / 100) {
       console.log(`${name} fell asleep after mowing the yard.`);
       return;
@@ -59,11 +59,11 @@ function waterGarden(name, callback) {
 
 function allChoresComplete(name) {
   console.log(`${name} finished all their chores!`);
-  fatigueLevel = 0; // Reset for next Saturday
+  fatigueLevel = 0;
 }
 
 function doSummerChores(name) {
-  fatigueLevel = 0; // Reset fatigue at start of day
+  fatigueLevel = 0;
   mowYard(name, (personName) => {
     weedEat(personName, (personName) => {
       trimHedges(personName, (personName) => {
